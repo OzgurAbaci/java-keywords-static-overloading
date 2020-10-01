79973e1544e3ac8af941e0f4664f6dfc64691180
OzgurAbaci <o.abaci@novi-education.nl>
2020-10-01 16:49:04 +0200

    Drie af

--- src/nl/novi/javaprogrammeren/three/Beer.java
+++ src/nl/novi/javaprogrammeren/three/Beer.java
@@ -2,8 +2,19 @@ package nl.novi.javaprogrammeren.three;
 
 public class Beer {
     private String brand;
+    static int numBeers = 0;
 
     public Beer(String brand) {
         this.brand = brand;
+        System.out.println("Biertje nummer " + ++numBeers + " is getapt");
+
+    }
+
+    public static int getTappedBeersAmount(){
+        return numBeers;
+    }
+
+    public int getAmountOfBeersTapped(){
+        return numBeers;
     }
 }
